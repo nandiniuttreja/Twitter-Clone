e1f51b4233f9961aaeffb73ec78e90fe4148e693
Parul2004 <parul1007.be21@chitkara.edu.in>
2023-08-27 12:21:38 -0700

    initial commit

--- script.js
+++ script.js
@@ -7,9 +7,35 @@ function addTodo(event) {
     const todoName = input.value;
 
     const li = document.createElement("li");
-    
+    const new_p = document.createElement("p");
+    const del_btn = document.createElement("button");
+    del_btn.classList.add("del");
+    del_btn.textContent = "Delete";
+    del_btn.style.color = "black";
+    del_btn.style.backgroundColor = "#f4f4f6";
+    del_btn.style.cursor = "pointer";
+
+    del_btn.style.padding = "6px";
+    del_btn.style.fontSize = "17px";
+    del_btn.style.fontWeight = "bold";
+    del_btn.style.boxShadow = "0px 1px 3px white";
+    del_btn.style.boxShadow = "-1px -1px 3px white";
+    del_btn.style.borderRadius = "6px";
+
+    // color: black;
+    // padding: 6px;
+    // border-radius: 10px;
+    // font-size: 17px;
+    // background-color: white;
+    // font-weight: bold;
+    // box-shadow: 0px 1px 3px white;
+    new_p.textContent = todoName;
+    li.appendChild(new_p);
+    li.appendChild(del_btn);
     list.appendChild(li);
-    li.textContent = todoName;
+
+
+    // li.textContent = todoName;
     li.style.color = "white";
     li.style.backgroundColor = "#7d95ad15";
     li.style.borderColor = "white";
@@ -23,4 +49,15 @@ function addTodo(event) {
     console.log(todoName);
 }
 
+function removeTodo(event) {
+    const clicked  = event.target;
+    const clickedIsDelBtn = clicked.matches(".del");
+    if(clickedIsDelBtn){
+        const delLi = clicked.parentElement;
+        delLi.style.display = "none";
+    }
+}
+
+
 btn.addEventListener("click",addTodo);
+list.addEventListener("click",removeTodo);
