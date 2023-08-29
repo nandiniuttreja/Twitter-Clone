fe06637841ac6e500e9f83dc0e16c4a4c795cd69
Chikki <nandiniuttreja@gmail.com>
2023-08-29 16:28:11 +0530

    concluded

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.getElementById("opac").style.opacity = "1";
 
 function saks() {
     document.getElementById("overlay").style.display = "flex"; 
-    document.querySelector("body").style.background = "#0c0d0d";
-    document.getElementById("opac").style.display = "none";
+    document.getElementById("opac").style.opacity = "0.3";  
+}
+
+function myFun() {
+    const x = document.getElementById("overlay");
+    x.style.display = "none";
+    document.getElementById("opac").style.opacity = "1"; 
 }
