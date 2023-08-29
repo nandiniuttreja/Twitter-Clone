b248c2b2fb2064fd8def6e7a9e34f286f04fa26d
Chikki <nandiniuttreja@gmail.com>
2023-08-29 15:54:51 +0530

    minor fixes

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.getElementById("opac").style.opacity = "1";
 
 function saks() {
     document.getElementById("overlay").style.display = "flex"; 
-    document.querySelector("body").style.background = "#191c1f";
+    document.querySelector("body").style.background = "#0c0d0d";
     document.getElementById("opac").style.display = "none";
 }
